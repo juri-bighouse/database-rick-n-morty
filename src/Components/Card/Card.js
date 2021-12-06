45025df470b90f03f7b6f242be787c4bfef05a91
juri casagranda <juri.carducci@gmail.com>
2021-12-06 22:34:44 +0100

    card style

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './Card.css';
-
+import Character from './Character';
 
 
 function Card(){
     return(
         <div className="card">
-            è una card
+            <img src={Character.image} alt={Character.name} />
+            <div className="card-info">
+                <p class="card-info__name">
+                    <span>Name:</span>
+                    {Character.name}
+                </p>
+                <p className="card-info__generic">
+                {Character.species} <span className="card-info__generic">from</span> {Character.location.name}
+                </p>
+        </div>
         </div>
     );
 }
