d8294909637521c1a986a44fc20c0d7e0188ceba
juri casagranda <juri.carducci@gmail.com>
2021-12-07 18:43:00 +0100

    Paginator v2

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import './App.css';
 import React from 'react';
 import Header from './Components/Header/Header.js';
 import Grid from './Components/Grid/Grid.js';
+import Pagina from './Components/Paginator/Paginator'
 class App extends React.Component {
   constructor(){
     super();
     this.state={
+      totalPages: 1,
+      currentPage: 1,
       isLoading: false,
       characters:[]
     }
@@ -14,10 +17,15 @@ class App extends React.Component {
   componentDidMount = () =>{
     this.fetchCharacters();
   }
+  componentDidUpdate = (prevProps,prevState) =>{
+    if(prevState.currentPage !== this.state.currentPage){
+      this.fetchCharacters();
+    }
+  }
   fetchCharacters = async () =>{
     try{
       this.setState({isLoading:true})
-      const response = await fetch("https://rickandmortyapi.com/api/character");
+      const response = await fetch('https://rickandmortyapi.com/api/character/?page='+ this.state.currentPage);
       const json = await response.json();
       this.setState({characters:json.results})
     }catch(e){
@@ -26,12 +34,16 @@ class App extends React.Component {
       this.setState({isLoading:false})
     }
   }
+  onPaginatorChange = (nextPage) => {
+    this.setState({currentPage:nextPage})
+  }
   render(){
-    const {characters,isLoading} = this.state;
+    const {characters,isLoading,currentPage,totalPages} = this.state;
     return (
     <div className="App">
      <Header />
      <Grid characters={characters} isLoading={isLoading}/>
+     <Pagina currentPage={currentPage} onChange={this.onPaginatorChange} totalPages={totalPages}/>
     </div>
   );
 }
